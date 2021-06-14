e1740962c0df64f1bd04ef2254ee000b50b68485
YanLovesCoding <oliviaenjoyslife@gmail.com>
2021-06-14 17:25:12 -0400

    fix deploy issue

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter  basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
